c6c8868be06cd998d4cf0a99681a194ba34f4fe9
Juan Tenezaca <tenj34@gmail.com>
2018-10-24 19:39:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,25 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,54 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    if logged_in?
+      redirect to '/tweets'
+    else
+		  erb :'/users/create_user'
+		end
+	end
+
+ 	post "/signup" do
+		user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
+		if user.save && user.username!= "" && user.email!="" && user.password!=""
+		  User.create(:username => params[:username], :password => params[:password], :email => params[:email])
+		  session[:user_id] = user.id
+			redirect to "/tweets"
+		else
+			redirect to "/signup"
+		end
+	end
+
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect to "/tweets"
+		else
+			redirect to "/signup"
+		end
+  end
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 
 end
